55da5e6b3f095f8923fdd420c3f18d6a9c8c140f
Nerio01 <nerios.cave@gmail.com>
2024-07-25 13:49:57 +0000

    installed npm packages and completed exercises 1-3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,5 @@
-const reverseString = function() {
-
-};
+const reverseString = (someString) => someString.split('').reverse().join('');
 
 // Do not edit below this line
 module.exports = reverseString;
+sat
