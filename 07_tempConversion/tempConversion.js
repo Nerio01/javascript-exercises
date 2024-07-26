1440c63814270489fcfc1d563beab615dab76b40
Nerio01 <nerios.cave@gmail.com>
2024-07-26 09:53:01 +0000

    solved exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+function round(value, precision) {
+  const multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function(temp) {
+  // To convert Fahrenheit to Celsius, subtract 32 from the Fahrenheit value and divide by 1.8.
+  const converted = (temp - 32) / 1.8;
+  return round(converted, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  // The Celsius to Fahrenheit formula is: °F = (°C × 1.8) + 32
+  const converted = (temp * 1.8) + 32;
+  return round(converted, 1);
 };
 
 // Do not edit below this line
